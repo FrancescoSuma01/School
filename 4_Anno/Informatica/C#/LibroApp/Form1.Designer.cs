2e9f00c2ab31750df316a24c1986c35a0e3b7ad1
FrancescoSuma01 <107925638+FrancescoSuma01@users.noreply.github.com>
2023-03-19 09:45:41 +0100

    Add files via upload

---
+++ 4_Anno/Informatica/C#/LibroApp/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace LibroApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitolo = new System.Windows.Forms.TextBox();
+            this.txtAutore = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.rdBtnAutore = new System.Windows.Forms.RadioButton();
+            this.rdBtnGenere = new System.Windows.Forms.RadioButton();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(297, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Genere";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Titolo";
+            // 
+            // txtTitolo
+            // 
+            this.txtTitolo.Location = new System.Drawing.Point(22, 31);
+            this.txtTitolo.Name = "txtTitolo";
+            this.txtTitolo.Size = new System.Drawing.Size(100, 23);
+            this.txtTitolo.TabIndex = 2;
+            // 
+            // txtAutore
+            // 
+            this.txtAutore.Location = new System.Drawing.Point(152, 30);
+            this.txtAutore.Name = "txtAutore";
+            this.txtAutore.Size = new System.Drawing.Size(100, 23);
+            this.txtAutore.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Autore";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Lime;
+            this.btnAdd.Location = new System.Drawing.Point(723, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(48, 45);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(22, 104);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(732, 94);
+            this.listBox1.TabIndex = 6;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(677, 393);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // rdBtnAutore
+            // 
+            this.rdBtnAutore.AutoSize = true;
+            this.rdBtnAutore.Location = new System.Drawing.Point(28, 229);
+            this.rdBtnAutore.Name = "rdBtnAutore";
+            this.rdBtnAutore.Size = new System.Drawing.Size(61, 19);
+            this.rdBtnAutore.TabIndex = 8;
+            this.rdBtnAutore.TabStop = true;
+            this.rdBtnAutore.Text = "Autore";
+            this.rdBtnAutore.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnGenere
+            // 
+            this.rdBtnGenere.AutoSize = true;
+            this.rdBtnGenere.Location = new System.Drawing.Point(28, 270);
+            this.rdBtnGenere.Name = "rdBtnGenere";
+            this.rdBtnGenere.Size = new System.Drawing.Size(62, 19);
+            this.rdBtnGenere.TabIndex = 9;
+            this.rdBtnGenere.TabStop = true;
+            this.rdBtnGenere.Text = "Genere";
+            this.rdBtnGenere.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(152, 225);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 23);
+            this.txtSearch.TabIndex = 10;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(333, 225);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.rdBtnGenere);
+            this.Controls.Add(this.rdBtnAutore);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAutore);
+            this.Controls.Add(this.txtTitolo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "LibroApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitolo;
+        private System.Windows.Forms.TextBox txtAutore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.RadioButton rdBtnAutore;
+        private System.Windows.Forms.RadioButton rdBtnGenere;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ 4_Anno/Informatica/C#/LibroApp/Libro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibroApp
+{
+    class Libro
+    {
+        private string _titolo;
+        private string _autore;
+        private string _genere;
+
+
+        public Libro()
+        {
+            Titolo = "";
+            Autore = "";
+            Genere = "";
+        }
+
+        public Libro(string t, string a, string g)
+        {
+            Titolo = t;
+            Autore = a;
+            Genere = g;
+        }
+
+        public string Description()
+        {
+            return Titolo + " \t " + Autore + " \t " + Genere;
+        }
+
+        public string Genere { get => _genere; set => _genere = value; }
+        public string Autore { get => _autore; set => _autore = value; }
+        public string Titolo { get => _titolo; set => _titolo = value; }
+    }
+}
